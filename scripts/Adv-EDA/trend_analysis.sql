3ea4aeef0cbf3d2b1472e77ebb03a81641561540
Krysis23 <parthborkute23@gmail.com>
2026-08-09 13:59:44 +0530

    advanced-eda

---
+++ scripts/Adv-EDA/trend_analysis.sql
@@ -0,0 +1,20 @@
+select
+year(order_date) as order_year,
+month(order_date) as order_month,
+sum(sales_amount) as total_sales,
+count(distinct customer_key) as total_customers,
+sum(quantity) as total_quantity
+from gold.fact_sales
+where order_date is not null
+group by year(order_date), month(order_date)
+order by year(order_date), month(order_date)
+
+select
+datetrunc(month,order_date) as order_date,
+sum(sales_amount) as total_sales,
+count(distinct customer_key) as total_customers,
+sum(quantity) as total_quantity
+from gold.fact_sales
+where order_date is not null
+group by datetrunc(month,order_date)
+order by datetrunc(month,order_date)
